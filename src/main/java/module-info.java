832978f2b0b6cf309c36c02b7c2d8ffc3ac08e11
Boris <krinitsky.B@yandex.ru>
2021-03-09 05:02:30 +0300

    add libs for converting file to image format

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.krinitsky {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.pdfbox;
+    requires org.apache.pdfbox.tools;
+    requires java.desktop;
 
     opens com.krinitsky to javafx.fxml;
     exports com.krinitsky;
