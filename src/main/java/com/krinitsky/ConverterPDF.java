69d67b4ba40f4b510486aece2e5b2219a5b0c5bf
Boris <krinitsky.B@yandex.ru>
2021-03-09 17:30:14 +0300

    add method for converting pdf file to image formats

--- src/main/java/com/krinitsky/ConverterPDF.java
+++ src/main/java/com/krinitsky/ConverterPDF.java
@@ -1,6 +1,14 @@
 package com.krinitsky;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
 public class ConverterPDF {
 
@@ -10,6 +18,24 @@ public class ConverterPDF {
     public ConverterPDF(File pathToSave, File pdfFile) {
         this.pathToSave = pathToSave;
         this.pdfFile = pdfFile;
+    }
+
+    /**
+     * Method convert pdf file to image format and save it.
+     *
+     * @param extension Extension for new file.
+     */
+    public void convertToImg(String extension) throws IOException {
+        PDDocument document = PDDocument.load(pdfFile);
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
+        for (int page = 0; page < document.getNumberOfPages(); ++page) {
+            BufferedImage bim = pdfRenderer.renderImageWithDPI(
+                    page, 300, ImageType.RGB);
+            ImageIOUtil.writeImage(
+                    bim,
+                    String.format("%s/%s%s", pathToSave, pdfFile.getName().split(".pdf")[0], extension), 300);
+        }
+        document.close();
     }
 
 }
