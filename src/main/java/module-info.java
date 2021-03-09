ac8991937528d1e71266ddf5b60bf4799378dc26
Boris <krinitsky.B@yandex.ru>
2021-03-09 18:32:17 +0300

    added libs for converting to html format

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.krinitsky {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.apache.pdfbox;
     requires org.apache.pdfbox.tools;
     requires java.desktop;
+    requires java.net.http;
+    requires org.apache.pdfbox;
+    requires net.sf.cssbox.pdf2dom;
 
     opens com.krinitsky to javafx.fxml;
     exports com.krinitsky;
